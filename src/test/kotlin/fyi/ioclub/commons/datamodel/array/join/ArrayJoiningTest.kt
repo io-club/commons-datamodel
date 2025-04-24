7b41b2c3643f7fdd8bd6221b45b003035df9f800
siiuml <siumlhahah@outlook.com>
2025-04-24 12:40:01 +0000

    Update license and version

--- src/test/kotlin/fyi/ioclub/commons/datamodel/array/join/ArrayJoiningTest.kt
+++ src/test/kotlin/fyi/ioclub/commons/datamodel/array/join/ArrayJoiningTest.kt
@@ -14,13 +14,16 @@
  * limitations under the License.
  */
 
-package fyi.ioclub.commons.datamodel.array.slice
+package fyi.ioclub.commons.datamodel.array.join
 
-import java.nio.ByteBuffer
+import fyi.ioclub.commons.datamodel.array.slice.simplified.get
 
-fun ByteBuffer.getArraySlice(length: Int = remaining()) = run {
-    val pos = position()
-    val slice = arraySliceOf(array(), arrayOffset() + pos, length)
-    position(pos + length)
-    slice
+fun testByteArrayJoining() {
+    val arr1 = "array1".encodeToByteArray()
+    val arr2 = "array2".encodeToByteArray()
+    println(join(arr1, 0, 3, arr2).decodeToString())
+    println(join(arr1, 3, arr2).decodeToString())
+    println((arr1[2, 3] + arr2[2, 3]).decodeToString())
 }
+
+fun main() = testByteArrayJoining()
