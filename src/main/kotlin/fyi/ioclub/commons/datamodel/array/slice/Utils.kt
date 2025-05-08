c4114b88a24b3739f1fa950625f030c9802b61e5
siiuml <siumlhahah@outlook.com>
2025-05-08 21:36:55 +0000

    Fix issue that `arraySliceOf` on `ArraySlice` can access to data which in array but out source array slice

--- src/main/kotlin/fyi/ioclub/commons/datamodel/array/slice/Utils.kt
+++ src/main/kotlin/fyi/ioclub/commons/datamodel/array/slice/Utils.kt
@@ -16,9 +16,9 @@
 
 package fyi.ioclub.commons.datamodel.array.slice
 
-internal fun checkIndexBounds(arrSize: Int, offset: Int, length: Int) {
+internal fun checkIndexBounds(capacity: Int, offset: Int, length: Int) {
     if (offset < 0) throw ArrayIndexOutOfBoundsException(offset)
-    (offset + length).let { if (it > arrSize) throw ArrayIndexOutOfBoundsException(it) }
+    (offset + length).let { if (it > capacity) throw ArrayIndexOutOfBoundsException(it) }
 }
 
 internal inline fun <A> slice(arr: A, off: Int, len: Int, sizeGetter: A.() -> Int, copier: A.(Int, Int) -> A) =
